c160163dc9557dc2b7743161fa5fdf54b65d2ae9
arielgelbard <ariel.gelbard@gmail.com>
2014-12-16 03:35:13 -0500

    Style Changes and Portfolio Pieces
    -Changed entire style of the website for a fresh, brand new look
    -Changed portfolio pieces to showcase the best projects created

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,15 +6,79 @@
 // Database of Portfolio Pieces
 // -----------------------------
 var portfolio = [
-{"id":"1","0":"1","title":"Emily Walkman","1":"Emily Walkman","profilePic":"piece1.png","2":"piece1.png"},
-{"id":"2","0":"2","title":"Ewaste Infographic","1":"Ewaste Infographic","profilePic":"piece3.png","2":"piece3.png"},
-{"id":"3","0":"3","title":"Marker","1":"Marker","profilePic":"piece4.png","2":"piece4.png"},
-{"id":"4","0":"4","title":"Not Far From the Tree","1":"Not Far From the Tree","profilePic":"piece2.png","2":"piece2.png"}];
+{"id":"1",
+"title":"AppSeed",
+"skills":"Android & iOS Developer",
+"profilePic":"piece1.png",
+},
 
-var portfolioDetails = [{"id":"1","0":"1","title":"Emily Walkman","1":"Emily Walkman","profilePic":"piece1.png","2":"piece1.png","button1Link":"#","3":"#","button1Title":"Github","4":"Github","button2Title":"NA","5":"NA","button2Link":"#","6":"#","role":"Design and Developer","7":"Design and Developer","skills":"Javascript and Titanium Studio","8":"Javascript and Titanium Studio","content":"NA","9":"NA","desktopScreenshot":"http:\/\/placehold.it\/426x266","10":"http:\/\/placehold.it\/426x266","mobileScreenshot":"http:\/\/placehold.it\/95x152","11":"http:\/\/placehold.it\/95x152"},
-{"id":"2","0":"2","title":"Ewaste Infographic","1":"Ewaste Infographic","profilePic":"piece3.png","2":"piece3.png","button1Link":"#","3":"#","button1Title":"Github","4":"Github","button2Title":"Visit","5":"Visit","button2Link":"http:\/\/arielgelbard.com\/infographic","6":"http:\/\/arielgelbard.com\/infographic","role":"Design and Developer","7":"Design and Developer","skills":"HTML5, CSS3, Jquery","8":"HTML5, CSS3, Jquery","content":"NA","9":"NA","desktopScreenshot":"http:\/\/placehold.it\/426x266","10":"http:\/\/placehold.it\/426x266","mobileScreenshot":"http:\/\/placehold.it\/95x152","11":"http:\/\/placehold.it\/95x152"},
-{"id":"3","0":"3","title":"Marker","1":"Marker","profilePic":"piece4.png","2":"piece4.png","button1Link":"#","3":"#","button1Title":"Github","4":"Github","button2Title":"Visit","5":"Visit","button2Link":"http:\/\/tristandarwent.com\/portfolio-detail.php?id=5","6":"http:\/\/tristandarwent.com\/portfolio-detail.php?id=5","role":"Developer","7":"Developer","skills":"Objective C","8":"Objective C","content":"NA","9":"NA","desktopScreenshot":"http:\/\/placehold.it\/426x266","10":"http:\/\/placehold.it\/426x266","mobileScreenshot":"http:\/\/placehold.it\/95x152","11":"http:\/\/placehold.it\/95x152"},
-{"id":"4","0":"4","title":"Not Far From the Tree","1":"Not Far From the Tree","profilePic":"piece2.png","2":"piece2.png","button1Link":"#","3":"#","button1Title":"Github","4":"Github","button2Title":"Visit","5":"Visit","button2Link":"http:\/\/arielgelbard.com\/notfarfromthetree","6":"http:\/\/arielgelbard.com\/notfarfromthetree","role":"Development","7":"Development","skills":"HTML5, CSS3, Jquery, PHP, mySQL","8":"HTML5, CSS3, Jquery, PHP, mySQL","content":"A non-profit organization that speacalizes in picking and sharing excess fruit.","9":"A non-profit organization that speacalizes in picking and sharing excess fruit.","desktopScreenshot":"\/assets\/images\/pieces\/piece2-desktop.png","10":"\/assets\/images\/desktop-notfar.png","mobileScreenshot":"\/assets\/images\/pieces\/piece2-mobile.png","11":"images\/mobile-notfar.png"}]
+{"id":"2",
+"title":"Ewaste Infographic",
+"skills":"Designer & Front-End Developer",
+"profilePic":"piece2.png",
+},
+
+{"id":"3",
+"title":"Not Far From the Tree",
+"skills":"Front-End & Back-End Developer",
+"profilePic":"piece3.png",
+},
+
+{"id":"4",
+"title":"Emily Walkman",
+"skills":"Designer & Mobile Developer",
+"profilePic":"piece4.png",
+}
+
+];
+
+var portfolioDetails = [
+{"id":"1",
+"title":"AppSeed: Turn Sketches into functioning prototypes",
+"button1Link":"https://www.kickstarter.com/projects/appseed/appseed-turn-sketches-into-functioning-prototypes",
+"button1Title":"Visit",
+"role":"Developer",
+"skills":"Java, Android Studio, Objective C, Xcode",
+"content":"This project focused on creating an app that would turn sketches into functioning prototypes.",
+"desktopScreenshot":"\/assets\/images\/pieces\/piece1-desktop.png",
+"mobileScreenshot":"\/assets\/images\/pieces\/piece1-mobile.png",
+},
+
+{"id":"2",
+"title":"Ewaste Infographic",
+"button1Title":"Visit",
+"button1Link":"http:\/\/arielgelbard.com\/infographic",
+"role":"Design and Developer",
+"skills":"HTML5, CSS3, Jquery",
+"content":"This project was produced in illustrator and photoshop and later coded into a interactive website.",
+"desktopScreenshot":"\/assets\/images\/pieces\/piece2-desktop.png",
+},
+
+{"id":"3",
+"title":"Not Far From the Tree",
+"button1Link":"https://github.com/arielgelbard/WEBD/tree/master/WEBD%20221/integrative",
+"button1Title":"Github",
+"button2Title":"Visit",
+"button2Link":"http:\/\/arielgelbard.com\/notfarfromthetree",
+"role":"Development",
+"skills":"HTML5, CSS3, Jquery, PHP, mySQL",
+"content":"A non-profit organization that speacalizes in picking and sharing excess fruit.",
+"desktopScreenshot":"\/assets\/images\/pieces\/piece3-desktop.png",
+"mobileScreenshot":"\/assets\/images\/pieces\/piece3-mobile.png",
+},
+
+{"id":"4",
+"title":"Emily Walkman",
+"profilePic":"piece1.png",
+"role":"Designer and Developer",
+"skills":"Javascript and Titanium Studio",
+"content":"A talented singer and song writer who wanted an application for her fans.",
+//<div class='caseStudy'><h1>Emily Walkerman Case Study</h1><div class='fiv'> <div class='fivLeft'> <h3>Introduction</h3><p>This project was for Emily Walkman, a musical artist who wanted an app so users can be able to listen to her music and browse other media content hassle free. The original design was great, but now times have changed; and a redesign was needed.</p></div><div class='fivRight'><img src='/assets/images/pieces/piece4/portfolio_improvement11.png' alt='#' /></div></div><div class='fiv'><div class='fivLeft'>  <h3>Style</h3><p>First off, the colour scheme and fonts needed an update and a better polish that would fit her persona.</p></div><div class='fivRight'>  <img src='/assets/images/pieces/piece4/portfolio_improvement12.png' alt='#' /></div></div><div class='fiv'> <div class='fivLeft'> <h3>Main Menu and Navigation</h3><p>In the original design, the app would first load and display a menu with options. Once an option was chosen, it would animate and slide a new page into the screen view. This experience felt like the user had to put effort into going and navigating from menu option to menu option.  Reaching the main menu options also wasn’t pleasant. Some sections didn’t have a top bar that integrated into the window with content appropriately. As well top bars seemed to be proving problematic and becoming a boring trend.</p></div><div class='fivRight'> <img src='/assets/images/pieces/piece4/portfolio_improvement11.png' alt='#' /></div></div><div class='fiv'> <div class='fivLeft'> <h3>Music Player</h3><p>The music player didn’t fit the theme and style of the application. There was also no entertainment solution for users who wants to listen to music and multitask within the app.</p></div><div class='fivRight'> <img src='/assets/images/pieces/piece4/portfolio_improvement16.png' alt='#' /></div></div><div class='fiv'> <div class='fivLeft'><h3>Social Media</h3><p>There are too many social media options to choose from, some are outdated streams of media, and users want all information at the same time. Unfortunately these sources came from all different sorts of unique news feeds.</p></div><div class='fivRight'><img src='/assets/images/pieces/piece4/portfolio_improvement15.png' alt='#' /></div></div><div class='fiv'> <div class='fivLeft'> <h3>Overall Foreseen Problem</h3><p>I had to think outside the box and attract users to wanting to go back and use the app every two weeks and be interactive with it. Users could always use a 3rd party music provider to download her music, go to youtube to watch her lyrical and/or music videos, and even casually browse social media sites to know more about any new information. </p></div><div class='fivRight'> <img src='/assets/images/pieces/piece4/portfolio_improvement14.png' alt='#' /></div></div><div class='fiv'> <div class='fivLeft'> <h3>Thinking Outside the Box and Triumphing</h3><p>I realized that all of the media presented wasn’t an all in one cohesive experience. That was the main goal. I wanted to think of the new, next big multimedia experience. Therefore, I wanted to create an all in one page experience so users would not to feel disconnected from page to page and task-to-task. Therefore, I edited, created and moved content and functionality in order to achieve a successful ux experience.</p></div><div class='fivRight'> <img src='/assets/images/pieces/piece4/portfolio_improvement.jpg' alt='#'/></div></div><div class='fiv'> <div class='fivLeft'> <h3>Style</h3><p>Through careful consideration, I had picked the best and most favorable colours, fonts, and created relatable icons that would be the best choice based on persona.</p></div><div class='fivRight'> <img src='/assets/images/pieces/piece4/portfolio_improvement1.jpg' alt='#'/></div></div><div class='fiv'> <div class='fivLeft'> <h3>Main Menu and Navigation: Scratch-It</h3><p>I first created a ‘main bar’ and fixed it to the bottom of the app for users to access the ‘menu options’ easier. This will allow users to access ‘menu options’ quicker and easier. The main menu has been re-designed and is presented in a neat, interactive way for users. The way it is styled, it is fun and visually appealing to view photos, the latest news, or chose a different music video media type. It easily fades in when the menu button is clicked and fades out once an option is selected.</p></div><div class='fivRight'> <img src='/assets/images/pieces/piece4/portfolio_improvement5.jpg' alt='#' class='portRight'/></div></div><div class='fiv' class='clear'><h3>Improving the Music Player</h3><p>In the music industry, the trend has always been to have a music video for the most popular song of an artist’s album. In some cases today, a whole album full of songs can be produced into theatrical music videos. In most cases today, songs have a Lyrical music video. As a great experience for the user, it was awesome to be able to integrate these interactive media videos. Instead of a boring music player with an album cover, users can enjoy watching music videos, following and/or interacting with the lyrics.</p><p>Another Significant decision taken was to move the music player controllers to the top. It helped create a simple solution to enable a larger screen real-estate and re-structure the layout based on the functionality of certain button. It just made sense to change the positions of these buttons to all sit together in the same similar section.</p></div><div class='fivRight2'> <img src='/assets/images/pieces/piece4/portfolio_improvement3.jpg' alt='#' class='portRight'/><img src='/assets/images/pieces/piece4/portfolio_improvement2.jpg' alt='#' class='portRight'/><img src='/assets/images/pieces/piece4/portfolio_improvement4.jpg' alt='#' class='portRight'/></div><div class='fiv'> <h3>Gathering Social Media Together</h3><p>There are so many social media sites and all of them are unique and serve a specific purpose. Usually users will have a news feed to check as their main source of information. Instead of having separate social media menu options, It was ideal to that these social media outlets can all fit into one news feed. Users have the option to filter certain social media outlets that they don’t desire to have in the feed.</p></div><div class='fivRight2'> <img src='/assets/images/pieces/piece4/portfolio_improvement6.jpg' alt='#' class='portRight'/><img src='/assets/images/pieces/piece4/portfolio_improvement7.jpg' alt='#' class='portRight'/></div><div class='fiv'> <h3>Conclusion</h3><p>There were a lot of layout issues and many dramatic improvements. I successfully implemented a new way to visualize music, have social media news and streams in a one page layout, and have an amazing ux user interface.</p></div><div class='fivRight2'><img src='/assets/images/pieces/piece4/portfolio_improvement9.jpg' alt='#' class='portRight'/><img src='/assets/images/pieces/piece4/portfolio_improvement10.jpg' alt='#' class='portRight'/></div></div>
+"desktopScreenshot":"\/assets\/images\/pieces\/piece4-desktop.png",
+"mobileScreenshot":"\/assets\/images\/pieces\/piece4-mobile.png",
+}
+
+];
 
 // -----------------------------
 // Wait for DOM Load
@@ -141,6 +205,7 @@ jQuery(function($) {
             //If a portfolio piece was found
             else{
               //Send Information pertaining to the portfilio piece
+
               this.model.set({
                 title: pPiece.title,
                 button1Title:pPiece.button1Title,
@@ -202,7 +267,13 @@ jQuery(function($) {
     if ($( this ).hasClass( "routeLink" )){
       e.preventDefault();
       //Animate to proper route
-      $("body").fadeOut(800,function(){      
+      $("body").fadeOut(800,function(){
+        if ( $(window).width() >= 750){
+          
+        }
+        else{
+          $("nav").slideToggle();
+        }
         App.router.navigate(link, { trigger: true });
         $("body").hide();
         $("body").fadeIn(800);         
